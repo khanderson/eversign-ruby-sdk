6bba345ccab1c4a84a157bfcd3d0e2fba59ce26a
David Sumulong <david@sumulong.com>
2019-03-01 11:20:33 +0800

    Include embedded_signing_url into signers model.

--- lib/eversign/models/signer.rb
+++ lib/eversign/models/signer.rb
@@ -2,6 +2,7 @@ module Eversign
 	module Models
 		class Signer
 	    attr_accessor :id, :name, :email, :order, :pin, :message, :deliver_email, :role  
+      attr_accessor :embedded_signing_url
 
       def initialize(name=nil, email=nil, role=nil)
         self.name = name
